b8dc3124d8c2d444277de7aafc2129d08b96f046
Moses Joseph <mosesejoseph@gmail.com>
2025-03-18 12:33:43 -0400

    - Updated jsdocumentation

--- src/commands/atlusEmbeds/metaphorEmbed.ts
+++ src/commands/atlusEmbeds/metaphorEmbed.ts
@@ -1,4 +1,10 @@
-import { EmbedBuilder, ApplicationCommandOptionType, Client, ChatInputCommandInteraction, MessageFlags  } from "discord.js";
+import {
+  EmbedBuilder,
+  ApplicationCommandOptionType,
+  Client,
+  ChatInputCommandInteraction,
+  MessageFlags,
+} from "discord.js";
 import { fetchMetaphorEnemyWeaknesses } from "../../queries/fetchMetaphorEnemyWeaknesses.js";
 import createMetaphorWeaknessChart from "../../createmetaphorWeaknessChart.js";
 import { fetchMetaphorEnemyStats } from "../../queries/fetchMetaphorEnemyStats.js";
@@ -6,16 +12,16 @@ import { MetaphorEnemyStats } from "src/interfaces.js";
 import { MetaphorEnemyWeaknesses } from "src/interfaces.js";
 
 /**
- * @module metaphorMonsterCommand
+ * @module weaknessChartCommand
  */
 
 /**
- * Slash command for fetching monster weaknesses.
+ * General Slash Command structure for fetching monster weaknesses and replying to the user with a visual representation.
  * @type {Object}
  * @property {string} name - The command name for the specific game.
  * @property {string} description - The description of the command.
  * @property {Array<Object>} options - The available options (user types in shadow/monster they are curious about).
- * @property {Function} callback - The function handling the command execution. 
+ * @property {Function} callback - The function handling the command execution.
  * We call query functions to the database using the option given by the user then send that data off into the weakness chart function to then reply with a weakness chart embed.
  */
 export default {
@@ -27,7 +33,7 @@ export default {
       description: "Name of monster",
       type: ApplicationCommandOptionType.String,
       required: true,
-      autocomplete: true
+      autocomplete: true,
     },
   ],
   //options: Object[],
@@ -36,20 +42,24 @@ export default {
   // botPermissions: Boolean,
 
   /**
-   * 
+   *
    * @async
    * @param {Client} client Represents an instance of the Atlus Discord Bot.
    * @param {ChatInputCommandInteraction} interaction Current instance of the chat input command inputted by user.
    * @returns Returns nothing but calls an async callback function if it passes all checks in handleCommands function.
    * @see {@link module:handleCommands}
    */
-  callback: async (client: Client, interaction: ChatInputCommandInteraction) => {
+  callback: async (
+    client: Client,
+    interaction: ChatInputCommandInteraction
+  ) => {
     // init dbResult & enemyStats
     let dbResult: MetaphorEnemyWeaknesses[];
     let enemyStats: MetaphorEnemyStats[];
 
     // Get the monster name from the interaction
-    const monsterName: string = interaction.options.get("monster-name").value as string;
+    const monsterName: string = interaction.options.get("monster-name")
+      .value as string;
 
     try {
       // Fetch weaknesses and stats
