a0b3edf54d6e2bb976b6b1971c1f1c429928e401
Moses Joseph <mosesejoseph@gmail.com>
2025-04-03 10:55:22 -0400

    Added proper links to bot

--- src/commands/userFeedback/feedback.ts
+++ src/commands/userFeedback/feedback.ts
@@ -100,7 +100,7 @@ export default {
 
       // Reply to the user confirming the submission
       await interaction.reply({
-        content: `Your feedback 👇 **\n\n${description}** \n\nhas been submitted successfully! You can submit feedback again in the next **${(cooldownTime / 1000 / 60 / 60).toFixed(1)} hours!**\nEnjoying Atlus Navigator? Leave it a rating on https://discordbotlist.com/ & https://top.gg/`,
+        content: `Your feedback 👇 **\n\n${description}** \n\nhas been submitted successfully! You can submit feedback again in the next **${(cooldownTime / 1000 / 60 / 60).toFixed(1)} hours!**\nEnjoying Atlus Navigator? Leave it a rating on [discordbotlist.com](https://discordbotlist.com/bots/atlus-navigator) & [top.gg](https://top.gg/bot/1328884093777350696) 😉`,
         flags: MessageFlags.Ephemeral, // This ensures the reply is only visible to the user who issued the command
       });
     } catch (error: any) {
